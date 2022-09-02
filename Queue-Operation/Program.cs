3933119fd710e806ec8f535ea17681f3588f7136
andr515i <andr515i@zbc.dk>
2022-09-02 10:09:40 +0200

    Add project files.

---
+++ Queue-Operation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
